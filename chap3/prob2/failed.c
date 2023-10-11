a9c6f707425bd5f1bae201e2f751c18049cd0534
ShinSeongHun <118865019+ShinSeongHun@users.noreply.github.com>
2023-10-11 11:34:24 +0900

    Update failed.c
    If I was succesful, I changed main.c :(

--- chap3/prob2/failed.c
+++ chap3/prob2/failed.c
@@ -13,35 +13,36 @@ int main()
 	   int i = 0, len, max = 0;
 
 	      while(i < MAXLINES && fgets(lines[i], MAXLINE, stdin) != NULL) {
-		            len = strlen(lines[i]);
-			          if (len > max) {
-					           max = len;
-						            copy(lines[i], longest);
-							          }
-				        i++;
-					   }
+		      len = strlen(lines[i]);
+		      if (len > max) {
+				max = len;
+				copy(lines[i], longest);
+				}
+			i++;
+		}
 	         
 
-	          for(int a = 0; a < MAXLINES; a++)
-			      {
-				              char tmp = 0;
-					              if(*lines[a] > *lines[a + 1])
-							              {
-									                  tmp = *lines[a];
-											              *lines[a] = *lines[a + 1];
-												                  *lines[a + 1] = tmp;
-														          }
-						          }
+		for(int a = 0; a < MAXLINES; a++)
+		{
+		char tmp = 0;
+			if(*lines[a] > *lines[a + 1])
+			{
+				tmp = *lines[a];
+			        *lines[a] = *lines[a + 1];
+				*lines[a + 1] = tmp;
+				}
+		}
 		      
-		      for (int j = 0; j < MAXLINES; j++) {
-			            printf("%s", lines[j]);
-				       }
-		         return 0;
+		for (int j = 0; j < MAXLINES; j++)
+		{
+			printf("%s", lines[j]);
+		}
+	return 0;
 }
 
 void copy(char from[], char to[])
 {
-	   int i = 0;
-	      while ((to[i] = from[i]) != '\0')
-		            ++i;
+	int i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
 }
