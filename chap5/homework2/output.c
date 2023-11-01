13695de481e94da1b4ea6ff5d4c316d48258a512
ShinSeongHun <118865019+ShinSeongHun@users.noreply.github.com>
2023-11-01 11:02:06 +0900

    :)

--- chap5/homework2/output.c
+++ chap5/homework2/output.c
@@ -8,7 +8,28 @@ int main(int argc, char *argv[])
 	{
 		return 1;
 	}
+	FILE *fp = fopen(argv[1], "r");
+	if (fp == NULL) 
+	{
+        	return 1;
+    	}
 
-	
+    	char line[100][100];
+	int count = 0;
 
+    	char buffer[100];
+    	while (fgets(buffer, sizeof(buffer), fp) != NULL) 
+	{
+        	strcpy(line[count], buffer);
+        	count++;
+    	}
+
+    	fclose(fp);
+
+    	for (int i = count - 1; i >= 0; i--) 
+	{
+        	printf("%s", line[i]);
+    	}
+
+    	return 0;
 }
