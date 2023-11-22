32506dafb857931ee81147500e49aaa30481111d
shin_seoung_hun <fdy1024@cu.ac.kr>
2023-11-22 05:45:08 +0000

    :)

---
+++ chap9/prob2/forkwait.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+	int pid, child, status;
+	printf("[%d] parent process start \n", getpid());
+	pid = fork();
+	if (pid == 0) 
+	{
+		printf("[%d] child process start \n", getpid());
+		exit(1);
+	}
+	child = wait(&status);
+	printf("[%d] child process %d done \n", getpid(), child);
+	printf("\t end code %d\n", status >> 8);
+}
